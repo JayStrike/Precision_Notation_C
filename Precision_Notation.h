ef9dd7e5a323b71f75bdab5c7f6f74732050c906
John G <63485354+JayStrike@users.noreply.github.com>
2024-05-21 13:07:21 -0400

    Create Precision_Notation.h

---
+++ Precision_Notation.h
@@ -0,0 +1,12 @@
+/*
+ * Header file for Precision_Notation.c
+ */
+
+#include <stdint.h>
+
+#ifndef PRECISION_NOTATION_H_
+#define PRECISION_NOTATION_H_
+
+// function declarations int foo(int x);
+
+#endif // PRECISION_NOTATION_H_
