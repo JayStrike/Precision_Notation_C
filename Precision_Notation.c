ef6b73caf8f809882dc839bd05fe0c9737df7b7b
John G <63485354+JayStrike@users.noreply.github.com>
2024-05-21 14:42:35 -0400

    Update Precision_Notation.c
    Not Complete Update. Non-functional

--- Precision_Notation.c
+++ Precision_Notation.c
@@ -1,3 +1,30 @@
 #include "Precision_Notation.h"
 
-// declare functions int foo(int x) return x + 1;
+// Checks precise number for overflows and adjusts accordingly
+void check_precise(Precise* num)
+{
+  // significand overflow check
+  for (int i = 0; i < 6; i++)
+  
+  return;
+}
+
+// Adds two precise numbers, avoiding over- or under-flow
+void add_precise(Precise a, Precise b, Precise* result)
+{
+  // check for over- and under-flow
+
+  // add a and b
+  Precise sum;
+
+  // transfer sum to result (so result can be initial address of a or b)
+  result->significandSign = sum.significandSign;
+  for (int i = 0; i < 6; i++)
+  {
+    result->significand[i] = sum.significand[i];
+  }
+  result->exponentSign = sum.exponentSign;
+  result->exponent = sum.exponent;
+
+  return;
+}
